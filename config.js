8a0584c3d532fac886ca900b35a462850354fd27
xLordkayx <westwalley061@gmail.com>
2024-11-07 06:54:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ global.caption = process.env.CAPTION || global.caption || "© 𝐋𝐨𝐫𝐝�
  
 
 global.devs =  "2347045505096" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "2347045505096";
 global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2347045505096";
 
 
